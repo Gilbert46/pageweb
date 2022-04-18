19f03286c0523cb128e851fac68f3e860710a475
Gilbert46 <67496732+Gilbert46@users.noreply.github.com>
2022-04-18 17:11:47 +0200

    Add files via upload

--- src/js/pass.js
+++ src/js/pass.js
@@ -12,5 +12,8 @@ function enterWeb() {
         document.getElementById("dis6").hidden = false;
         document.getElementById("dis7").hidden = false;
         document.getElementById("dis8").hidden = false;
+        document.getElementById("dis9").hidden = false;
+        document.getElementById("dis10").hidden = false;
+        document.getElementById("dis11").hidden = false;
     }
 }
