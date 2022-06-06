bc1e9f3aec679a39c560bca6de0f927e97688a29
Gilbert46 <67496732+Gilbert46@users.noreply.github.com>
2022-06-06 18:19:24 +0200

    Add files via upload

---
+++ src/js/pass.js
@@ -0,0 +1,19 @@
+var ad;
+
+
+function enterWeb() {
+    ad = document.getElementById("admin").value;
+
+    if (ad == "987654") {
+        document.getElementById("dis2").hidden = false;
+        document.getElementById("dis3").hidden = false;
+        document.getElementById("dis4").hidden = false;
+        document.getElementById("dis5").hidden = false;
+        document.getElementById("dis6").hidden = false;
+        document.getElementById("dis7").hidden = false;
+        document.getElementById("dis8").hidden = false;
+        document.getElementById("dis9").hidden = false;
+        document.getElementById("dis10").hidden = false;
+        document.getElementById("dis11").hidden = false;
+    }
+}
